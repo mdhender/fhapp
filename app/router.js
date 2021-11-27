4016142fc1e25af08b0045cd677949198b27752a
Michael D Henderson <mdhender@mdhender.com>
2021-11-27 23:33:59 +0000

    fixed error in path to config

--- app/router.js
+++ app/router.js
@@ -1,5 +1,5 @@
 import EmberRouter from '@ember/routing/router';
-import config from 'inbox/config/environment';
+import config from 'fhapp/config/environment';
 
 export default class Router extends EmberRouter {
   location = config.locationType;
